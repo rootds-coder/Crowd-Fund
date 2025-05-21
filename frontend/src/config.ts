3ff092799a04e7aa9e2a02b8afe793e41776394a
dhruv <dhruv9671267714@gmail.com>
2025-05-21 15:33:05 +0100

    Delete all files

--- frontend/src/config.ts
+++
@@ -1,8 +0,0 @@
-declare const process: {
-  env: {
-    REACT_APP_API_URL?: string;
-  }
-};
-
-// Temporarily hardcoding the URL to ensure it's using the correct backend
-export const API_URL = 'https://test-ydrz.onrender.com/api'; 
